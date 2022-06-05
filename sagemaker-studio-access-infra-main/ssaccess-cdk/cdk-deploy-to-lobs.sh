d00be3f35d850fda885c8d0e5c640d566bfd2f0a
rvvittal <49964623+rvvittal@users.noreply.github.com>
2022-06-04 23:46:42 -0700

    Update cdk-deploy-to-lobs.sh
    remove account info

--- sagemaker-studio-access-infra-main/ssaccess-cdk/cdk-deploy-to-lobs.sh
+++ sagemaker-studio-access-infra-main/ssaccess-cdk/cdk-deploy-to-lobs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # cdk-deploy-to-lobs.sh
-./cdk-deploy-to.sh 368766719129 us-east-1 --profile lob1 "$@" || exit
-./cdk-deploy-to.sh 145102640261 us-east-1 --profile lob2 "$@"
+./cdk-deploy-to.sh <lob1-account> us-east-1 --profile lob1 "$@" || exit
+./cdk-deploy-to.sh <lob2-account> us-east-1 --profile lob2 "$@"
